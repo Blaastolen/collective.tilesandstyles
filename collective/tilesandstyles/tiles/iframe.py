5cac51c1e83c86cb2f08bcb126c7531df65e166c
Martin O Reistadbakk <martin@blaastolen.com>
2016-04-01 18:05:03 +0200

    added iframe tile

---
+++ collective/tilesandstyles/tiles/iframe.py
@@ -0,0 +1,27 @@
+from DateTime import DateTime
+from zope.interface import Interface
+from zope import schema
+
+from plone.tiles import PersistentTile
+from zope.interface import implements
+
+
+
+class IIFrameTile(Schema):
+    iframe = schema.SourceText(
+        title= u"Responsive Iframe",
+        description=u"Paste your <iframe tag here",
+        required=True
+    )
+
+
+
+class IFrameTile(PersistentTile):
+    """HTML tile."""
+
+    implements(IIFrameTile)
+
+
+    def __call__(self):
+        iframe = self.data.get('iframe', u'')
+        return u"""<html><body><div class="videowrapper">"""+iframe+"""</div></body></html>"""
