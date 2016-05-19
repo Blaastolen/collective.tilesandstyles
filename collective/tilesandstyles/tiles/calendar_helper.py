b74f562aa98c20df6848f1143018532421f697de
Martin O Reistadbakk <martin@blaastolen.com>
2016-05-19 20:09:46 +0200

    change dateformat

---
+++ collective/tilesandstyles/tiles/calendar_helper.py
@@ -0,0 +1,27 @@
+from zope.i18n import translate
+from Products.CMFCore.utils import getToolByName
+from Products.Five import BrowserView
+
+from Products.CMFPlone.i18nl10n import monthname_english, monthname_msgid, \
+                                       monthname_msgid_abbr, weekdayname_msgid, \
+                                       weekdayname_english \
+
+
+
+class CalendarHelperView(BrowserView):
+    def month_name(self, month_no):
+        return translate(monthname_msgid(str(month_no)),
+                         'plonelocales',
+                          context=self.request,
+                          default=monthname_english(month_no))
+
+
+    def day_name(self, day_no):
+        if day_no == 7:
+            day_no = 1
+        else:
+            day_no +=1
+        return translate(weekdayname_msgid(str(day_no)),
+                         'plonelocales',
+                          context=self.request,
+                          default=weekdayname_english(day_no))
